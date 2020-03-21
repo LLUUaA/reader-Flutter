632ceae3876d5d4ae1f5efe8025ddba4d3a7665f
lluuaa <1012758795@qq.com>
2020-03-21 21:42:57 +0800

    load page

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -38,31 +38,91 @@ class _MyApp extends State<MyApp> {
 class LoadPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size contentSize = MediaQuery.of(context).size;
     return SafeArea(
       child: Container(
         // height: ,
         color: Colors.white,
         child: Stack(
           children: <Widget>[
-            Center(
-              child: Text(
-                '青莲一页',
-                style: TextStyle(
-                  color: Colors.blueAccent,
-                  decoration: TextDecoration.none
+            Container(
+              child: Center(
+                child: Image.asset('lib/assets/image/book_lover.png'),
+              ),
+            ),
+            Positioned(
+              bottom: 60.0,
+              right: 5.0,
+              child: CountDown(),
+            ),
+            Positioned(
+              bottom: 40.0,
+              width: contentSize.width,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  '青莲一页',
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none),
                 ),
               ),
             ),
-            Container(
-              child: Text(
-                'Copyright © 2020-present WuQingSong',
-                style: TextStyle(color: Colors.grey[600], 
-                fontSize: 13.0,
-                 decoration: TextDecoration.none),
+            Positioned(
+              width: contentSize.width,
+              bottom: 10.0,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Copyright © 2019-present WuQingSong',
+                  style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 13.0,
+                      decoration: TextDecoration.none),
+                ),
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+// count down
+class CountDown extends StatefulWidget {
+  final int secends;
+  CountDown({this.secends = 3});
+  @override
+  _CountDown createState() => _CountDown(this.secends);
+}
+
+class _CountDown extends State<CountDown> {
+  final int secends;
+  _CountDown(this.secends) {
+    this._init();
+  }
+  // init
+  void _init() async {
+    await Future.delayed(Duration(seconds: this.secends));
+    this.jumpTo();
+  }
+
+  // to home
+  void jumpTo() {
+    Navigator.pushNamed(context, '/');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      padding: EdgeInsets.all(0),
+      onPressed: this.jumpTo,
+      child: Text(
+        '跳过',
+        style: TextStyle(letterSpacing: 2.0),
       ),
     );
   }
